f9f5e6ab462258f1cf22842783f0f11a0832a2fc
donCESAR12345 <sprit152009@gmail.com>
2022-11-08 22:41:23 -0500

    Añadido logo a recursos públicos e índice de prueba para página inicial
    Signed-off-by: donCESAR12345 <sprit152009@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Components
-import NavBar from './components/navbar';
+// import NavBar from './components/navbar';
 
 // Pages
 import Home from './pages';
-import Register from './pages/register';
-import Login from './pages/login';
+// import Register from './pages/register';
+// import Login from './pages/login';
 
 function App() {
   return (
     <Router>
-      <NavBar />
-      <div className="top-space"></div>
+      {/* <NavBar /> */}
+      {/* <div className="top-space"></div> */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/register" element={<Register />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
     </Router >
   );
