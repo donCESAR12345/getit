d9120c74edb6018e0d15a85fb9554908e88efe3b
donCESAR12345 <sprit152009@gmail.com>
2022-11-12 15:28:40 -0500

    Removed styles from register and added into .css
    Signed-off-by: donCESAR12345 <sprit152009@gmail.com>

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const oneDay = 1000 * 60 * 60 * 24;
 
 // Conexión a la base de datos
 const client = new Client();
-const REGISTER_QUERY = "SELECT email FROM users"
+const INSERT_QUERY = "INSERT INTO users(email, password, is_admin, first_name, last_name, country, birthdate)";
 const LOGIN_QUERY = "SELECT email, password FROM users";
 client.connect();
 
@@ -54,9 +54,13 @@ app.get("/", function(req, res) {
 
 // Renderizar página de registro
 app.get("/register", function(req, res) {
+  let error = req.query.error;
+  if (error === undefined) {
+    error = 0;
+  }
   res.render("register", {
     name: "register",
-    error: 0
+    error: error
   });
 });
 
@@ -72,59 +76,85 @@ app.get("/login", function(req, res) {
   });
 });
 
-// Manejar solicitudes POST en registro
-app.post("/register", registerNewUser);
+// Manejar solicitudes POST
 app.post("/login", loginSession);
+app.post("/register", registerNewUser);
 
 function loginSession(req, res) {
   let body = req.body;
-  let query = LOGIN_QUERY + " WHERE email='" + body.email + "';";
+  let result = findUserInDB(body.email);
 
+  if (result.rows.length > 0) {
+    if (result.rows[0].password === body.password) {
+      session = req.session;
+      session.userid = body.username;
+      console.log(session.userid + " has logged in!");
+      res.redirect("/");
+    }
+    else {
+      console.log("Access denied!");
+    }
+  }
+  else {
+    console.log("User not found!");
+  }
+  res.redirect("/login?error=1");
+}
+
+function registerNewUser(req, res) {
+  let body = req.body;
+  let result = findUserInDB(body.email);
+
+  if (result.rows.length === 0) {
+    console.log(body);
+  }
+}
+
+function findUserInDB(email) {
+  const query = `SELECT email, password FROM users 
+    WHERE email=${email};`;
   client.query(query, (err, result) => {
     if (err) {
       console.error(err);
       return;
     }
     if (result.rows.length > 0) {
-      if (result.rows[0].password === body.password) {
-        session = req.session;
-        session.userid = body.username;
-        console.log(req.session + " has logged in!");
-        res.redirect("/");
-      }
-      else {
-        console.log("Access denied!");
-      }
+      return result;
     }
     else {
       console.log("User not found!");
-      res.redirect("/login?error=1");
+      return;
     }
   });
 }
 
-function registerNewUser(req, res) {
-  let body = req.body;
-  let query = REGISTER_QUERY + " WHERE email='" + body.email + "';";
-
-  if (query.rows.length > 0) {
-    res.render("/register", {
-      error: 1
-    });
-  }
-  else {
-    console.log("Usuario registrado");
-  }
+function insertUserInDB(data) {
+  const query = `INSERT INTO users(
+    email, password, is_admin, first_name,
+    last_name, country, birthdate)
+    VALUES(
+    ${data.email}, ${data.password}, ${data.is_admin},
+    ${data.first_name}, ${data.last_name},
+    ${data.country}, ${data.birthdate}
+    );`;
+  client.query(query, (err, result) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    else {
+      return result;
+    }
+  });
 }
-
-//app.post("/login", (req, res) => {
-//  let body = req.body;
-//  let query = LOGIN_QUERY + " WHERE email='" + body.email + "'";
-//
-//
-//  res.redirect(CLIENT_URI + "/login");
-//});
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log("Server started on port " + PORT));
+
+/*
+  ERRORES:
+  0: No hubo errores,
+  1: Usuario no existente o contraseña incorrecta,
+  2: Usuario ya existente (registro),
+*/
